5192d9a2c3ce9467b83b676c6e82ba309d13e123
b21ricni <b21ricni@student.his.se>
2022-05-10 14:34:44 +0200

    Corrected DatabaseHelper and DatabaseTables, name was wrong Added Function for fetching mountain

--- app/src/main/java/com/example/mysqlite/MainActivity.java
+++ app/src/main/java/com/example/mysqlite/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.mysqlite;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private SQLiteDatabase database;
+    private DatabaseHelper databaseHelper;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        databaseHelper = new DatabaseHelper(this);
+        database = databaseHelper.getWritableDatabase();
+        addMountain();
+
     }
+
+    private void addMountain(){
+        ContentValues values = new ContentValues();
+        values.put(DatabaseTables.Mountain.COLUMN_NAME_NAME, "Kinnekulle");
+        values.put(DatabaseTables.Mountain.COLUMN_NAME_HEIGHT, 352);
+        database.insert(DatabaseTables.Mountain.TABLE_NAME, null, values);
+    }
+
+
 }
